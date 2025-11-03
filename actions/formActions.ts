ab282935cc804bdbbffc23ed54e7afa3e826da7b
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2025-11-03 14:45:53 +0100

    swtich import logic to API route

--- actions/formActions.ts
+++ actions/formActions.ts
@@ -3,23 +3,7 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function importFile(formData: FormData){
-    if(!formData) return;
 
-    const file = formData.get("text") as File;
-    const text = await file.text();
-    
-    if(!text) return;
-
-    const jobs = text.split('-');
-
-    await Promise.all(
-        jobs.map(title => db.job.create({ data: { title, status: "APPLIED" }}))
-    );
-
-    revalidatePath("/");
-    redirect("/"); 
-}
 
 export async function bulkUpdateJobStatuses(formData: FormData) {
   const ids = formData.getAll("ids") as string[];
--- components/FileImportButtons.tsx
+++ components/FileImportButtons.tsx
@@ -1,9 +1,13 @@
-import { importFile } from "@/actions/formActions";
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 
 export function FileImportForm() {
   return (
-    <form action={importFile} className="flex gap-2 justify-end">
+    <form
+      action="/api/import"
+      method="POST"
+      encType="multipart/form-data"
+      className="flex gap-2 justify-end"
+    >
       <input
         type="file"
         id="text"
